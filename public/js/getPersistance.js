9e0835f9e36b285f9198fc8c3167a94739843ad0
kennedn <kennedn@msn.com>
2024-05-22 13:49:17 +0100

    Revert color change

--- public/js/getPersistance.js
+++ public/js/getPersistance.js
@@ -1,12 +1,1 @@
-let backgroundColor = sessionStorage.getItem('lastColor') || `rgba(28,129,126, 0.7)`;
-
-// Extract the numeric RGB values from the input string
-let rgbValues = backgroundColor.match(/\d+/g);
-
-// Check if the input string was in the expected RGB format
-if (!rgbValues || rgbValues.length !== 3) {
-    document.body.style.backgroundColor = backgroundColor;
-} else {
-    let [r, g, b] = rgbValues.map(Number);
-    document.body.style.backgroundColor = `rgba(${r}, ${g}, ${b}, 0.7)`;
-}
+document.body.style.backgroundColor = sessionStorage.getItem('lastColor') || '#1c817e';
